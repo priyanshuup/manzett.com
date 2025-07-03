ce935e1c592d1d9f260012dc33995c521e1f00fc
unknown <priyanshu9555@gmail.com>
2025-07-03 23:58:28 +0530

    swipper added

---
+++ assets/js/swiper.js
@@ -0,0 +1,36 @@
+const swiper = new Swiper('.testimonial-main', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  loop: true,
+  loopedSlides: 5, // should equal your number of slides
+  speed: 1000,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+});
+
+
+const heroSwiper = new Swiper('.heroswipe.swiper', {
+  slidesPerView: 1,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.hero-swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  }
+});
