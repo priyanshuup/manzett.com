b40a7d8e5ddc16ba9e8f47f6045cdad2ad5e26e4
unknown <priyanshu9555@gmail.com>
2025-07-05 02:53:27 +0530

    about page added

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -34,3 +34,19 @@ const heroSwiper = new Swiper('.heroswipe.swiper', {
     dynamicBullets: true,
   }
 });
+
+
+  const mySwiper = new Swiper(".myAwardsSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
